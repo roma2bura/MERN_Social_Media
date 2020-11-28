7cdad4f76ba47661d2ffd7e5334d7d4c743b46fa
Roman Bura <r2bcloud@gmail.com>
2020-11-27 17:56:40 -0800

    Profile page upd

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -1,8 +1,11 @@
 import React, {Fragment, useEffect} from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
+import {Link} from 'react-router-dom'
 import Spinner from '../layout/Spinner'
 import {getProfileById} from '../../actions/profile';
+import ProfileTop from './ProfileTop';
+import ProfileAbout from './ProfileAbout';
 
 
 const Profile = ({ 
@@ -13,10 +16,19 @@ const Profile = ({
 }) => {
     useEffect(() => {
         getProfileById(match.params.id);
-    }, [getProfileById]);
-    return <div>
-            profile
-        </div>;
+    }, [getProfileById, match.params.id]);
+    return <Fragment>
+            {profile === null || loading ? <Spinner /> : <Fragment>
+                <Link to="/profiles" className="btn btn-light">Back to Profiles</Link>
+                {auth.isAuthenticated && auth.loading === false && auth.user._id === profile.user._id && (<Link to="/edit-profile" className="btn btn-dark">Edit Profile</Link>)}
+
+                <div class="profile-grid my-1">
+                <ProfileTop profile={profile} />
+                <ProfileAbout profile={profile} />
+                </div>
+
+                </Fragment>}
+        </Fragment>;
     
 };
 
